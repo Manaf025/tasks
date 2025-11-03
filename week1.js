63b0295d4fb808ec519358b7d3c6107edaae6697
Manaf25 <ssirinov8@gmail.com>
2025-11-03 13:47:30 +0400

    update

--- week1.js
+++ week1.js
@@ -1,11 +1,61 @@
-const ad = "Ali";
-const yas = 14;
 
-console.log(`Hello, ${ad}`);
+//1
+console.log("Hello, JavaScript");
 
-if (yas >= 18) {
-  console.log("Sizin yaşınız uyğundur");
+
+
+
+
+
+const firstName = "Ali";
+const lastName = "Huseynov";
+
+
+const fullName = `${firstName} ${lastName}`;
+console.log("Full name:", fullName);
+
+
+
+
+//5
+let movies = ["Inception", "Matrix", "Avatar", "Titanic", "Joker"];
+console.log("Movies:", movies);
+
+
+movies.push("Interstellar");
+
+
+movies.pop();
+
+
+console.log("Final movies list:", movies);
+
+
+
+//6
+const score = 85;
+if(score >= 90)
+{
+  console.log("A");
+} else if(score >= 80){
+  console.log("B");
 } else {
-  console.log("Yaşınız bunun üçün uyğundur deyil");
+  console.log("C or below");
 }
+
+
+
+// 7
+console.log("Numbers 1 to 20:");
+for(let i=1; i<=20; i++){
+  console.log(i);
+}
+
+console.log("Even numbers 1 to 20:");
+for(let i=1; i<=20; i++){
+  if(i % 2 === 0){
+    console.log(i);
+  }
+}
+
 
