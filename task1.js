14cb89abe4c1cdad82c1f2253b594501613b2165
Manaf25 <ssirinov8@gmail.com>
2025-11-03 12:33:58 +0400

    End of week 1 challenge

--- task1.js
+++ task1.js
@@ -1,1 +1,11 @@
-console.log("hello world 2") 
+const ad = "Ali";
+const yas = 14;
+
+console.log(`Hello, ${ad}`);
+
+if (yas >= 18) {
+  console.log("Sizin yaşınız uyğundur");
+} else {
+  console.log("Yaşınız bunun üçün uyğundur deyil");
+}
+
