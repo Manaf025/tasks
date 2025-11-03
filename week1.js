28c0b3e805ca4898d51d9c9b3c860a323ac54f7f
Manaf25 <ssirinov8@gmail.com>
2025-11-03 13:05:03 +0400

    javascript_roadmap_week1

---
+++ week1.js
@@ -0,0 +1,11 @@
+const ad = "Ali";
+const yas = 14;
+
+console.log(`Hello, ${ad}`);
+
+if (yas >= 18) {
+  console.log("Sizin yaşınız uyğundur");
+} else {
+  console.log("Yaşınız bunun üçün uyğundur deyil");
+}
+
