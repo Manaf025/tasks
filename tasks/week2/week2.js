f4557480af0f0a417fa39941e822d46327c677c7
Manaf25 <ssirinov8@gmail.com>
2025-12-14 13:17:34 +0400

    refactor: Commented out old example and added new counter with data

--- tasks/week2/week2.js
+++ tasks/week2/week2.js
@@ -1,3 +1,25 @@
+// onsole.log(" Timerlar Misalı Başladı ");
+
+
+// setTimeout(() => {
+//     console.log("Runs once after 1s");
+// }, 1000);
+
+
+// const id = setInterval(() => {
+//     console.log("Tick");
+// }, 500);
+
+
+// setTimeout(() => {
+//     clearInterval(id);
+//     console.log("Interval dayandırıldı (3 saniyə keçdi)");
+//     console.log("Timerlar Misalı Bitdi ");
+// }, 3000);
+
+
+
+
 const EventEmitter = require("events");
 
 
