2f231c4e4d3f66206872f177ea45da264af8e9b1
Manaf25 <ssirinov8@gmail.com>
2025-12-15 14:50:16 +0400

    update: week1.js faylı yeni tapşırıq kodu ilə yeniləndi

--- tasks/week1/week1.js
+++ tasks/week1/week1.js
@@ -1,9 +1,48 @@
-// console.log("hello world");
+// console.log("hello world");      //TASK 4
 // let myage =22
 // console.log(myage);
 // myage =24
 // console.log(myage);
 
-const budget =100 
-console.log(budget);
-const budget //error verir cunki const daxilinde iki eyni adli box yaratmaq olmur
+// const budget =100 
+// console.log(budget);
+// const budget //error verir cunki const daxilinde iki eyni adli box yaratmaq olmur
+
+
+
+
+// let message ="Hava Yaqislidir";    //TASK5
+// let number = 17;
+// let price =1.99
+// console.log(message);
+// console.log(number);
+// console.log(price);
+
+// let isRaining = true;
+// let isSunny = false;
+
+// console.log(isRaining);
+// console.log(isSunny);
+
+// let toplama =15+2    //TASK6
+// let cixma =5-2
+// let vurma =9*8
+// let bolme =5/2
+
+// console.log(toplama);
+// console.log(cixma);
+// console.log(vurma);
+// console.log(bolme);  
+
+// let firstname="Manaf"   //TASK 7
+// let lastname="Mamoyev"
+// let fullname=firstname+" "+lastname
+
+// console.log(fullname);
+
+const myfirstName="Manaf"   //HOMEWORK
+const mylastName="Mamoyev"
+const myAge=18
+const myCity="Baku"
+
+console.log("My name is"+" "+myfirstName+" "+mylastName+" "+"I am"+" "+ myAge+" "+"Years old and I live in"+" "+myCity);
