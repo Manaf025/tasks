ee302d2996ce9c4aa1009f839c1a6cf7b7f3e976
Manaf25 <ssirinov8@gmail.com>
2025-12-22 12:02:38 +0400

    Week 3 tapsiriqlari ?lav? olundu

---
+++ tasks/week3/week3.js
@@ -0,0 +1,28 @@
+//let names=["Manaf","Tahir","Elcin"]
+//names.push("Gunay")
+// console.log(names.length);
+// console.log(names);
+//console.log(names[3]);    //TASK1
+
+// let numbers=[10,11,12,13,17];
+// let total=0;
+// for(let i=0;i<numbers.length;i++)
+// {
+//     total=total+numbers[i]
+// }
+// console.log(total);     //TASK2
+
+// let nomers=[10,5,17,9,21,90]
+
+// for(i=0;i<nomers.length;i++)
+// {
+//     if(nomers[i]>10)
+//     {
+//         console.log(nomers[i]);
+//     }
+// }
+
+
+
+
+
